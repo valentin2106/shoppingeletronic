e317192f87b5a943f5a239274775446e9322f2da
Fernando <fernandovalentim.cja@hotmail.com>
2022-02-09 15:33:22 -0300

    screen-login

---
+++ src/Components/Login/style.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+export const StyleLogin = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
