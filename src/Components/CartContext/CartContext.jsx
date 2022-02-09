d0d617d4fa3d799fdc80adb26d338ac7f713d9f2
Fernando <fernandovalentim.cja@hotmail.com>
2022-02-09 12:47:57 -0300

    createfunctions

---
+++ src/Components/CartContext/CartContext.jsx
@@ -0,0 +1,63 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState({});
+ useEffect(()=>{
+ const cartLocal = window.localStorage.getItem('cart')
+ if(cartLocal){
+     setCart(JSON.parse(cartLocal))
+ }
+ },[])
+  const addToCart = (product) => {
+    setCart((old) =>{
+        let quantity = 0
+        if(old[product.id]){
+            quantity = old[product.id].quantity
+        }
+     const newCart = {
+      ...old,
+      [product.id]: {
+          quantity: quantity + 1,
+          product,
+      },
+    }
+    window.localStorage.setItem('cart', JSON.stringify(newCart))
+    return newCart
+});
+  };
+  const removeCart = (productId) => {
+      setCart(old =>{
+          const newCart = {}
+          Object.keys(old).forEach(id =>{
+              if(id !== productId){
+                  newCart[id] = old[id]
+              }
+          })
+          window.localStorage.setItem('cart', JSON.stringify(newCart))
+          return newCart
+      })
+  } 
+  const change = (productId) => {
+    setCart(old =>{
+        const newCart = {}
+        Object.keys(old).forEach(id =>{
+            if(id !== productId){
+                newCart[id] = old[id]
+            }
+        })
+        window.localStorage.setItem('cart', JSON.stringify(newCart))
+        return newCart
+    })
+} 
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+export const useCart = () =>{
+    const cart = useContext(CartContext);
+    return cart
+}
